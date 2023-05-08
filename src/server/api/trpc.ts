76e8f743615f6e5073f2e2f55db02cab549f2235
Tomasz Steć <tomeczekstecc@gmail.com>
2023-05-08 18:35:24 +0200

    films model

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -117,6 +117,9 @@ const isAdmin = t.middleware((opts) => {
 
   const {req} = ctx;
 
+  console.log('ctx', ctx?.user);
+  console.log('ctx', ctx?.user?.role);
+
   if (!ctx.user || ctx.user.role !== 'ADMIN') {
     throw new TRPCError({code: 'UNAUTHORIZED', message: 'Brak uprawnień'});
   }
