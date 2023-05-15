0712dff9d89b38451068ff117b4e697525f2ef2e
Tomasz Steć <tomeczekstecc@gmail.com>
2023-05-15 23:23:45 +0200

    landing page

--- src/pages/blog/components/Blog.tsx
+++ src/pages/blog/components/Blog.tsx
@@ -9,7 +9,7 @@ const Blog = ({posts, refetch}) => {
     <div className={'mb-32 grid grid-cols-1 gap-y-10 gap-x-20 lg:grid-cols-2'}>
       {posts?.slice(0, 2).map((post) => (
         <div key={post.id}>
-          <BlogCard featured refetch={refetch} key={post.id} post={post}/></div>
+          <BlogCard featured refetch={refetch} key={post.id} post={post} nosubtitle={false}/></div>
       ))
       } </div>
     <h2 className={'mb-4 text-2xl '}>Pozostałe wpisy</h2>
